d41b582c446dd9fc42d11f65a116694aa3249636
Marino.Malinconico <marino.malinconico@nttdata.com>
2025-11-14 13:01:07 +0100

    added initial files

---
+++ src/main/java/com/companyName/coreMicroservices/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package com.companyName.coreMicroservices.repository;
+
+import com.companyName.accountMicroservices.repository.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+    @Query(value="SELECT * FROM accounts WHERE FK_USER=:FkUser", nativeQuery = true)
+    List<Account> getAllAccountPerUser(@Param("FkUser")String FkUser);
+
+    List<Account> findByFkUser(@Param("FkUser")String FkUser);
+
+    List<Account> findAll();
+
+}
