917e2a0b260c7650586cae1aa12490fd9d755a6e
Marino.Malinconico <marino.malinconico@nttdata.com>
2025-12-15 17:05:07 +0100

    cleaned code

--- src/main/java/com/companyName/coreMicroservices/repository/InvoiceRepository.java
+++ src/main/java/com/companyName/coreMicroservices/repository/InvoiceRepository.java
@@ -8,9 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    /* DEPRECATO
-    @Query(value="SELECT * FROM invoices WHERE INVOICE_NUMBER=:invoice_number", nativeQuery = true)
-    List<Invoice> getAllInvoicePerUser(@Param("invoice_number")Long invoice_number);*/
 
     List<Invoice> findByinvoiceNumber(@Param("invoice_number")Long invoice_number);
 
